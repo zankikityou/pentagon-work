59407f8fb9eb6159f613a320e55daf9eafc570d0
zankikityou <332758092+zankikityou@users.noreply.github.com>
2026-09-23 20:47:09 +0900

    Refresh app cache for visual summary update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='pentagon-work-v1';
+const CACHE_NAME='pentagon-work-v2';
 const APP_SHELL=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
